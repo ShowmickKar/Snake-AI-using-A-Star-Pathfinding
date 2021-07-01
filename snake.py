443ba262ecd85ee7689307bb8c61e1c5b37f9f53
Showmick Kar <showmick12@gmail.com>
2021-07-02 00:51:30 +0600

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 import pygame
 from cell import Cell
 
-RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 
 """
@@ -11,7 +11,7 @@ If the snake eats food then don't pop the last element
 
 class Snake:
     def __init__(self):
-        self.color = RED
+        self.color = GREEN
         self.body = 30
         self.velocity = 30
         self.direction = [0, 0]
@@ -19,8 +19,6 @@ class Snake:
         self.container = [Cell(300, 500)]
 
     def move(self):
-        if self.eating_stage:
-            print("YES")
         """
         Use the direction attribute to navigate path
         """
@@ -36,22 +34,20 @@ class Snake:
         else:
             self.container.pop()
 
-        if self.container[0].x + self.body < 0:
-            self.container[0].x = 600
-        if self.container[0].x > 600:
-            self.container[0].x = 0
-        if self.container[0].y + self.body < 0:
-            self.container[0].y = 600
-        if self.container[0].y > 600:
-            self.container[0].y = 0
+        # if self.container[0].x + self.body < 0:
+        #     self.container[0].x = 600
+        # if self.container[0].x > 600:
+        #     self.container[0].x = 0
+        # if self.container[0].y + self.body < 0:
+        #     self.container[0].y = 600
+        # if self.container[0].y > 600:
+        #     self.container[0].y = 0
 
     def draw(self, window):
         for cell in self.container:
-            pygame.draw.rect(
-                window, (53, 51, 102), (cell.x, cell.y, self.body, self.body)
-            )
+            pygame.draw.rect(window, (0, 0, 0), (cell.x, cell.y, self.body, self.body))
             pygame.draw.rect(
                 window,
-                cell.color,
+                self.color,
                 (cell.x + 5, cell.y + 5, self.body - 10, self.body - 10),
             )
